ff347eefd9873aace31c6aef669b41bc47f446cb
Maxim Tam <maximtea.cloud@gmail.com>
2017-10-27 21:29:59 -0400

    Removed Jupyter notebook

--- main/init.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('ahoy.db')
-cur = conn.cursor()
-cur.execute('CREATE TABLE user (id INTEGER NOT NULL, username VARCHAR, password VARCHAR, PRIMARY KEY (id))')
-conn.commit()
-conn.close()
